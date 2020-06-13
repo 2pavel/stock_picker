c4a13ad8b31d730d9189852f643ec516dc230085
2pavel <pawelnwz@gmail.com>
2020-06-13 14:49:52 +0200

    outputs profit and 2 element array of days indexes

---
+++ stockpicker.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+def stock_picker(prices_array)
+  best_trade = Array.new
+  current_best = 0
+  prices_array.each_with_index do |price, day|
+    for i in 0...prices_array.length
+      trade = prices_array[i + day] - price if prices_array[i + day] != nil
+
+      #binding.pry
+      if trade > current_best
+        current_best = trade
+        best_trade = [day, i]
+
+      #binding.pry
+      
+      end
+    end
+  end
+  p current_best
+  p best_trade
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
